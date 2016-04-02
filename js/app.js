e5f339e930fa5dd675dc33b4ebc57581464be8b3
Yonathan Ayenew <yonayen@gmail.com>
2016-04-01 22:32:15 -0700

    fix mouseenter

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
-	$('.ryu').on("mouseenter", function() {
-		// alert('mouse entered .ryu div');
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	});
-	$('.volume-control').on("click", function() {
-		$('.volume-control').toggleClass("fa-volume-up fa-volume-off")
-	})
-});
+// $(document).ready(function() {
+// 	$('.ryu').on("mouseenter", function() {
+// 		// alert('mouse entered .ryu div');
+// 		$('.ryu-still').hide();
+// 		$('.ryu-ready').show();
+// 	});
+// 	$('.volume-control').on("click", function() {
+// 		$('.volume-control').toggleClass("fa-volume-up fa-volume-off")
+// 	})
+// });
