001055e39d7432f76eee3085c0c362391ac96392
Yonathan Ayenew <yonayen@gmail.com>
2016-04-02 05:22:08 -0700

    mousedown, mouseup, hadouken animations

--- js/app.js
+++ js/app.js
@@ -3,12 +3,35 @@ $(document).ready(function() {
 		// alert('mouse entered .ryu div');
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
-	});
-	$('.ryu').on("mouseleave", function() {
+	})
+	.on("mouseleave", function() {
 		$('.ryu-still').show();
 		$('.ryu-ready').hide();
 	});
+	$('.ryu').on("mousedown", function() {
+		// console.log('mousedown');
+		$('.ryu-throwing').show();
+		$('.ryu-ready').hide();
+		$('.hadouken').show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			}
+		);
+		// $('.')
+		// play hadouken sound
+		// animate hadouken
+	})
+	.on("mouseup", function() {
+		// console.log('mouseup');
+		$('.ryu-thowing').hide();
+		$('.ryu-ready').show();
+		// $('.hadouken').hide();
+	});
+
 	$('.volume-control').on("click", function() {
 		$('.volume-control').toggleClass("fa-volume-up fa-volume-off")
-	})
+	});
 });
